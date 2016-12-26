1a94916230e3f18a0ba5150a548051e7a2ec215a
diogoberti88 <diogoberti88@gmail.com>
2016-12-26 16:07:06 -0200

    Adicao de mais models(nutrients e meal Itens), inclusão de uma notebook para adicionar nutrients em produtos (que são diet items), entre outras...

--- models/models.py
+++ models/models.py
@@ -11,7 +11,7 @@ class dietfacts(models.Model):
     calories = fields.Integer('Calories')
     servingsize = fields.Float('Serving Size')
     lastupdated = fields.Date('Last Updated')
-    
+    meal_nutrient_ids = fields.One2many('product.template.nutrient', 'product_id')
     
 class dietfacts_res_users_meal(models.Model):
     _name = 'res.users.meal'
@@ -21,18 +21,24 @@ class dietfacts_res_users_meal(models.Model):
     user_id = fields.Many2one('res.users','Meal user')
     notes = fields.Text('Meal notes')
     totalcalories = fields.Integer(string="Total Calories", store=True, compute="_calccalories")
+    totalitems = fields.Integer(string="Meal items", compute="_calccalories", store=True) 
     
-    @api.one
+    @api.multi
     @api.depends('item_ids','item_ids.servings')
     def _calccalories(self):
         currentcalories = 0
+        currentitem = 0
         
         for item in self.item_ids:
             caloriestocalculate = item.item_id.calories
             servsize = item.servings
             currentcalories = currentcalories + (caloriestocalculate * servsize)
-        
+            currentitem += 1
+            
         self.totalcalories = currentcalories  
+        self.totalitems = currentitem
+            
+        
     
 class dietfacts_res_users_mealitem(models.Model):
     _name = 'res.users.mealitem'
@@ -44,7 +50,22 @@ class dietfacts_res_users_mealitem(models.Model):
                                store=True, readonly=True)
     
     
+    dailyvalue = fields.Float('Daily value')
+
+class dietfacts_product_nutrient(models.Model):
+    _name = 'product.nutrient'
+    name = fields.Char('Nutrient name')
+    uom_id = fields.Many2one('product.uom', 'Unity of Measure')
+    description = fields.Text('Description')
+    #meal_item_id = fields.Many2one('product.template','Meal')
     
+
+class dietfacts_product_template_nutrient(models.Model):
+    _name = 'product.template.nutrient'
+    nutrient_id = fields.Many2one('product.nutrient', string='Nutrient')
+    product_id = fields.Many2one('product.template', string='Diet Item')
+    value = fields.Float('Value')
+    dailypercentage = fields.Float('Daily Percentage')
     #dietitem = fields.Boolean('DietItem')
     
     
